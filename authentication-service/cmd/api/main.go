e83ffee8d349ce255229cc0d0639d966b5a54fbc
ica1130 <ilijakrilovic@gmail.com>
2024-04-20 23:03:06 +0200

    feat: created logic to add and connect to DB

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -6,9 +6,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgx/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const webPort = "80"
+
+var counts int64
 
 type Config struct {
 	DB     *sql.DB
@@ -18,10 +26,18 @@ type Config struct {
 func main() {
 	log.Println("Starting authentication service...")
 
-	// TODO connect to DB
+	// connect to DB
+
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Can't connect to Postgres database.")
+	}
 
 	// set up the configuration
-	app := Config{}
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -31,5 +47,44 @@ func main() {
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// we are creating this function because we want to retry connecting to the database if the connection fails
+// ecpecially because we are going to be running this in a container, sometimes the database might not be ready
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Error connecting to database: ", err)
+			counts++
+		} else {
+			log.Println("Connected to database successfully")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for 2 seconds..")
+		time.Sleep(2 * time.Second)
+		continue
 	}
 }
