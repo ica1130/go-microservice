c512b209ead85a4b52df9fd9276cc1c07de7b807
ica1130 <ilijakrilovic@gmail.com>
2024-04-26 13:30:43 +0200

    feat: Added conn func for rabbitmq to listener-service

---
+++ listener-service/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	// amqp stands for Advanced Message Queuing Protocol
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	// try to connect to rabbitmq
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+	log.Println("Connected to RabbitMQ")
+
+	// start listening for messages
+
+	// create consumer
+
+	// watch the que and consume events
+}
+
+func connect() (*amqp.Connection, error) {
+	// sometimes it takes a while to connect to the server
+	// so we will write also a "backoff" function
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// dont continue until rabbitmq is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			fmt.Println("RabbitMQ is not ready yet.")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off..")
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
+}
