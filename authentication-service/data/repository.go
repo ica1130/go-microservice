316d5bd28049eb352cee1b7af969c3f3491a58e2
ica1130 <ilijakrilovic@gmail.com>
2024-05-01 19:59:17 +0200

    fix: models.go

--- authentication-service/data/repository.go
+++ authentication-service/data/repository.go
@@ -1,5 +1,8 @@
 package data
 
+// Repository is the interface that defines the methods that must be implemented by a
+// repository. This is used to allow us to switch between different storage solutions
+// without changing the code that uses the repository.
 type Repository interface {
 	GetAll() ([]*User, error)
 	GetByEmail(email string) (*User, error)
