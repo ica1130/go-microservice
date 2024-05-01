fe34485716e9b224b9acfd410240c2d5bc865980
ica1130 <ilijakrilovic@gmail.com>
2024-05-01 20:16:09 +0200

    test: auth-service handlers

--- authentication-service/cmd/api/setup_test.go
+++ authentication-service/cmd/api/setup_test.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"authentication/data"
 	"os"
 	"testing"
 )
 
+var testApp Config
+
 // This is the entry point for the test suite
 // It will run before any tests are run
 func TestMain(m *testing.M) {
+
+	repo := data.NewPostgresTestRepository(nil)
+	testApp.Repo = repo
 
 	// This will run the tests
 	os.Exit(m.Run())
